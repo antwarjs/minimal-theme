b5992ab6f6ce92150cc05c8ddf61f5a2105e097a
eldh <andreas.eldh@gmail.com>
2015-03-10 22:44:50 +0100

    Nav

--- Body.jsx
+++ Body.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
-
+var _ = require('lodash');
+var Router = require('react-router');
+var config = require('config');
+var Link = Router.Link;
 var Body = React.createClass({
 
 	render: function() {
+		var links = _.map(config.site.theme.navigation, function(link) {
+			return <li><Link to={link.path} key={link.path}>{link.title}</Link></li>
+		});
 		return (
-			<main>{this.props.children}</main>
+			<main>
+				<ul>{links}</ul>
+				{this.props.children}
+			</main>
 		);
 	}
 
