8a822d3f78cd4c2f0413a22c27501c9ec8cb5b56
eldh <andreas.eldh@gmail.com>
2015-06-14 20:05:53 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+	handlers: {
+    sectionIndex: function() {
+      return require('./SectionIndex.jsx');
+    },
+    sectionItem: function() {
+      return require('./SectionItem.jsx');
+    },
+    body: function() {
+      return require('./Body.jsx');
+    },
+  }
+}
