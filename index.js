f63b502c18159c948e43ac419c217e07eeb3ebda
Juho Vepsalainen <bebraw@gmail.com>
2015-06-16 08:29:25 +0300

    Remove redundant handlers
    These aren't needed after all.

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-module.exports = {
-  handlers: {
-    sectionIndex: function() {
-      return require('./SectionIndex.jsx');
-    },
-    sectionItem: function() {
-      return require('./SectionItem.jsx');
-    },
-    body: function() {
-      return require('./Body.jsx');
-    },
-  }
-}
+module.exports = {};
